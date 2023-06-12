8db3bbb1753a8d83c27e735f2fe9839742d663e9
Blickenderfer <ecblickenderfer@gmail.com>
2023-06-12 16:25:08 -0400

    added category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,20 +17,43 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findOne({
+      where: {id: req.params.id},
+      include: [{model: Product}]
+    })
+    res.status(200).json(categoryData)
+  } catch (error) {
+    res.status(500).json(error)
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const category = await Category.create(req.body)
+    res.status(200).json(category)
+  } catch (error) {
+    res.status(500).json(error)
+  }
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const category = await Category.update(req.body, {
+      where: {id: req.params.id}
+    })
+    res.status(200).json(category)
+  } catch (error) {
+    res.status(500).json(error)
+  }
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 
